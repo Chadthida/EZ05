04d2ddd15784b2f477422ce955d4a0533d0c8378
Yuttana ChuanChuChai <646051200129@mail.rmutk.ac.th>
2024-01-18 20:45:20 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EZ05"
+include(":app")
+ 
